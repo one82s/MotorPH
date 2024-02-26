5f540880c1537e21d912e3beaefe1fac3851d3aa
Ortega <Ortega@LAPTOP-ORAP623D>
2024-02-27 07:57:56 +0800

    first commit

---
+++ src/motorph/EmployeeModel.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package motorph;
+
+/**
+ *
+ * @author Ortega
+ */
+public abstract class EmployeeModel {
+
+    //Declaring an Employee object array
+    protected Employee[] employees;
+
+    protected abstract Employee[] getEmployeeModelList();
+
+}
---
+++ src/motorph/SalaryOnHoursWorked.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package motorph;
+
+/**
+ *
+ * @author Ortega
+ */
+public class SalaryOnHoursWorked {
+
+    public double getSalaryOnHoursWorked(double hourlyRate) {
+        return hourlyRate * 160;
+    }
+
+}
